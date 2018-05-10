b5f5326d985f7633d04420db6e1194dccf0c80c1
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-05-10 15:55:09 +0200

    Update text-bill.test.js

--- tests/text-bill.test.js
+++ tests/text-bill.test.js
@@ -17,4 +17,19 @@ describe('textBill Widget', function(){
 
     assert.equal(text.smses(), 3.00);
   });
+  it ('should add total for calls and sms Bill', function(){
+  var text = BillLogic();
+  text.calculate('sms');
+  text.calculate('sms');
+  text.calculate('sms');
+  text.calculate('sms');
+
+  text.calculate('call');
+  text.calculate('call');
+  text.calculate('call');
+  text.calculate('call');
+
+  assert.equal(text.total(), 14.00);
 });
+});
+
